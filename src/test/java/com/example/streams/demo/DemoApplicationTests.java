18d29af0a3626598d0c4404de85db89e52aebafc
Juliherms de Lima Vasconcelos <jlv@REDENCAO-015591.cesar-ldap.intranet>
2020-10-06 11:12:51 -0300

    implementando filtros com streams

--- src/test/java/com/example/streams/demo/DemoApplicationTests.java
+++ src/test/java/com/example/streams/demo/DemoApplicationTests.java
@@ -8,11 +8,14 @@ import java.util.function.Consumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.streams.demo.domais.Curso;
 import com.example.streams.demo.util.ComparadorTamanho;
 import com.example.streams.demo.util.ImprimeNaLinha;
 
 @SpringBootTest
 class DemoApplicationTests {
+
+	List<Curso> cursos = new ArrayList<Curso>();
 
 	@Test
 	void contextLoads() {
@@ -117,15 +120,15 @@ class DemoApplicationTests {
 		palavras.add("Juliherms Vasconcelos");
 		palavras.add("Fred Vasconcelos");
 		palavras.add("Celia Maria");
-		
+
 		// exemplo de método funcional com lambda
 		palavras.sort((String s1, String s2) -> Integer.compare(s1.length(), s2.length())); // novo no java 8
-		
+
 		// utilizando lambda
 		palavras.forEach(s -> System.out.println(s));
 
 	}
-	
+
 	/**
 	 * Testar ordenacao e listagem com lambda utilizando a Integer comparator
 	 */
@@ -137,12 +140,61 @@ class DemoApplicationTests {
 		palavras.add("Fred Vasconcelos");
 		palavras.add("Celia Maria");
 
-		//dado um objeto chama o método do objeto
-		//dado uma string chama o método da string
+		// dado um objeto chama o método do objeto
+		// dado uma string chama o método da string
 		palavras.sort(Comparator.comparing(s -> s.length())); // compara o tamanho de cada string
-		//palavras.sort(Comparator.comparing(String::length)); -> isso aqui é chamado de metodo reference
+		// palavras.sort(Comparator.comparing(String::length)); -> isso aqui é chamado
+		// de metodo reference
 
 		// utilizando lambda
 		palavras.forEach(s -> System.out.println(s));
+	}
+
+	/**
+	 * Testar ordenacao de lista com lambda
+	 */
+	@Test
+	public void testarOrdenarCursos() {
+
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+
+		cursos.sort(Comparator.comparing(Curso::getAlunos)); // method reference
+
+		cursos.forEach(c -> System.out.println(c.getNome()));
+	}
+
+	/**
+	 * Testar filtro por quantidade de alunos com stream
+	 */
+	@Test
+	public void testarImprimirCursosAcimaDe100Alunos() {
+
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+
+		cursos.stream().filter(c -> c.getAlunos() >= 100).forEach(c -> System.out.println(c.getNome()));
+	}
+
+	/**
+	 * Testar filtro por quantidade de alunos com stream utilizando o total de
+	 * alunos por curso
+	 */
+	@Test
+	public void testarImprimirCursosAcimaDe100AlunosTotalizado() {
+
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+
+		int sum = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(c -> c.getAlunos()).sum();
+		
+		System.out.println(sum);
+
 	}
 }
